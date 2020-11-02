6dccdb77ae4f919f5dd9ae6676046097d3979b4f
ellymaimon <eliranmaimon@gmail.com>
2020-11-01 16:34:21 -0800

    udpate the get customers query to include phone & address

--- src/db/queries.js
+++ src/db/queries.js
@@ -1,13 +1,20 @@
 import gql from "graphql-tag";
 
 export const getAllCustomers = gql`
- {
-   allCustomers {
-     data {
-       _id
-       firstName
-       lastName
-     }
-   }
- }
+{
+	allCustomers {
+		data {
+			_id
+			firstName
+			lastName
+			telephone
+			address {
+				street
+				city
+				state
+				zipCode
+			}
+		}
+	}
+}
 `;
