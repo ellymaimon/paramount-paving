05cbe4ba15434c2a2a45f15edd8089dc413b89da
ellymaimon <eliranmaimon@gmail.com>
2020-11-01 16:36:48 -0800

    render customer card in customer list

--- src/CustomersList.js
+++ src/CustomersList.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { getAllCustomers } from "./db/queries";
+import CustomerCard from "./CustomerCard";
 
-export function CustomersList() {
+export default function CustomersList() {
 	const { data, loading } = useQuery(getAllCustomers);
+
 	if (loading) {
 		return "Loading...";
 	}
+
 	return (
 		<ul>
 			{data.allCustomers.data.map((customer) => {
-				return <li key={customer._id}>{customer.firstName} {customer.lastName}</li>;
+				return (
+					<CustomerCard
+						key={customer._id}
+						customer={customer}
+					/>
+				);
 			})}
 		</ul>
 	);
