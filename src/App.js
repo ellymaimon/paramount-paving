9262e0faadf3328b435254843797fe7256b8d7d7
ellymaimon <eliranmaimon@gmail.com>
2020-11-01 16:35:40 -0800

    render CustomerForm in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./db/client";
-import { CustomersList } from "./CustomersList";
+import CustomersList from "./CustomersList";
+import CustomerForm from "./CustomerForm";
 
 export default function App() {
  return (
@@ -9,6 +10,7 @@ export default function App() {
      <div style={{ padding: "5px" }}>
        <h3>Paramount Paving</h3>
        <CustomersList/>
+       <CustomerForm/>
      </div>
    </ApolloProvider>
  );
